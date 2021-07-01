7a143acc7e5eac9a1351c5ed08ed62af844436b0
Ekene Oguikpu <ekeneoguikpu@gmail.com>
2021-07-01 06:17:47 +0100

    changing fleet direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -116,8 +116,14 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+    
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_aliens(self):
+        self._check_fleet_edges()
         self.aliens.update()
     
     
