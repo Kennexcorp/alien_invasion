6dd073ffcc4737fa22e2c2590185a5c5d91fd272
Ekene Oguikpu <ekeneoguikpu@gmail.com>
2021-07-01 06:30:44 +0100

    detected if aliens reach screen edges and also change their direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -119,6 +119,12 @@ class AlienInvasion:
     
     def _check_fleet_edges(self):
         for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+            
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
 
