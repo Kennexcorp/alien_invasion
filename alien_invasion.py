17737ad48130a1509071c44dfff0aaa1bf9814df
Ekene Oguikpu <ekeneoguikpu@gmail.com>
2021-07-02 12:31:00 +0100

    display player game level

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,6 +70,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             self.aliens.empty()
             self.bullets.empty()
@@ -136,6 +137,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase Level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def update_screen(self):
         # Redraw the screen during each pass through the loop
