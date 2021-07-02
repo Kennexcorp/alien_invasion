895f5f66432c6be5663247bc139826bccde009e1
Ekene Oguikpu <ekeneoguikpu@gmail.com>
2021-07-02 10:56:02 +0100

    added game resetting feature

--- alien_invasion.py
+++ alien_invasion.py
@@ -55,6 +55,23 @@ class AlienInvasion:
 
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.reset_stats()
+            self.stats.game_active = True
+
+            self.aliens.empty()
+            self.bullets.empty()
+
+            self._create_fleet()
+            self.ship.center_ship()
+
+
                     
     def _check_keydown_events(self, event):
 
